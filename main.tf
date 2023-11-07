aa447b76bcddb6b1a0a8f63f373b0218220233e0
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-11-07 20:08:50 +0530

    new iac code

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ resource "azurerm_lb" "load_balancer" {
         for_each = each.value.frontend_ip_configuration
         content {
         name = frontend_ip_configuration.value.name
-        zones = each.value.zones
-        public_ip_address_id = var.public_ip_output[frontend_ip_configuration.value.public_ip_name].id
-        subnet_id = var.vnet_subnet_output[frontend_ip_configuration.value.subnet_name].id
-        private_ip_address_allocation = frontend_ip_configuration.value.private_ip_address_allocation
+        zones = frontend_ip_configuration.value.zones == [] ? [] : frontend_ip_configuration.value.zones
+        public_ip_address_id = frontend_ip_configuration.value.public_ip_name == null ? null : var.public_ip_output[frontend_ip_configuration.value.public_ip_name].id
+        subnet_id = frontend_ip_configuration.value.subnet_name == null? null : var.subnet_output[frontend_ip_configuration.value.subnet_name].id
+        private_ip_address_allocation = frontend_ip_configuration.value.private_ip_address_allocation == null? null : frontend_ip_configuration.value.private_ip_address_allocation
     }
     }
 }
